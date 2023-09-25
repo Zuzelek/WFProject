c4b8bb3233fa0775ba0f89d2e0f0c8e54faa9dec
Alan <alan35691@gmail.com>
2023-09-25 20:28:52 +0100

    Third Commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,16 +1,15 @@
 /* GET 'home' page */
 const homelist = function(req, res){
-res.render('locations-list', { title: 'Locations' });
+res.render('locations-list', { title: 'Home' });
 };
 /* GET 'Location info' page */
 const locationInfo = function(req, res){
-res.render('location-info', { title: 'Location info' });
+res.render('index', { title: 'Location info' });
 };
 /* GET 'Add review' page */
 const addReview = function(req, res){
 res.render('index', { title: 'Add review' });
 };
-
 module.exports = {
 homelist,
 locationInfo,
